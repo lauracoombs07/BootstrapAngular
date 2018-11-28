30fd53e9a47706ad5bf8b1a3991ffbb31323b87e
Laura <laura.coombs07@gmail.com>
2018-11-28 14:46:40 -0700

    component changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
