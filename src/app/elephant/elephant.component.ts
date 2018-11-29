3f9d9064f1f271eb4de57005deb981810b092a67
Shane Jacobson <shane-jacobson@hotmail.com>
2018-11-29 13:55:41 -0700

    Shane Routing

---
+++ src/app/elephant/elephant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-elephant',
+  templateUrl: './elephant.component.html',
+  styleUrls: ['./elephant.component.css']
+})
+export class ElephantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
