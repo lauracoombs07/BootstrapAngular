a91f61d690c96e5327d468fb5837c66d9783d659
Austin Quattrocchi-Smuda <AustinJSmuda@icloud.com>
2018-12-03 22:02:44 -0700

    carl component added - it doesnt work yet, but who does?

---
+++ src/app/carlcomponent/carl.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+@Component({
+    selector: 'app-carl',
+    templateUrl: './carl.component.html',
+    styleUrls: ['./carl.component.css']
+  })
+  export class CarlComponent implements OnInit {
+  
+    constructor() { }
+    // <div><img src= "http://oi63.tinypic.com/1zxur7o.jpg" class = PIC></div>
+
+    ngOnInit() {
+    }
+  
+  }
